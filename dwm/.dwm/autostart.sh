c136a07fc56a118dd8d5ad23ad69d21e71fab4e1
Drew Herron <herron@mailbox.org>
2022-07-29 13:13:56 -0700

    Slock and brightness control script

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+sh $HOME/Pictures/wallpaper/.fehbg &
+
+xautolock -time 10 -locker slock &
+
+# Open st, cd to Music, and set class as music_term (tag 6 for me)
+st -d ~/Music -c music_term &
+
 dwmblocks
