97cd9eec44ea472516fdfd87141883c3d02a0111
Drew Herron <herron@mailbox.org>
2022-07-30 00:12:24 -0700

    Get slock to turn off screen

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sh $HOME/Pictures/wallpaper/.fehbg &
 
-xautolock -time 10 -locker slock &
+xautolock -time 10 -locker slock_off &
 
 # Open st, cd to Music, and set class as music_term (tag 6 for me)
 st -d ~/Music -c music_term &
