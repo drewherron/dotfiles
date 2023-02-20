633c92b5220b231cc317d14069bd6b679e2274bf
Drew Herron <herron@mailbox.org>
2023-02-20 10:23:07 -0800

    Autostart dual-monitor script

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -3,7 +3,9 @@
 #sh $HOME/Pictures/wallpaper/.fehbg &
 xsetroot -solid black &
 
-xautolock -time 10 -locker slock_off &
+sh $HOME/.screenlayout/dual-monitor.sh &
+
+#xautolock -time 10 -locker slock_off &
 
 # Open st, cd to Music, and set class as music_term (tag 6 for me)
 st -d ~/Music -c music_term &
